654dddf168cc30c4fdc24149dbeff9e23847a220
Lalit Sahu <98540540+VITianLalit@users.noreply.github.com>
2024-01-12 20:30:35 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/app_intro_with_indicators_application_flutter_ui_challenge_21/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_intro_with_indicators_application_flutter_ui_challenge_21
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
